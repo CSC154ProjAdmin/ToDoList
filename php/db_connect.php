aad8d8d1fec6600562198505fdd7adfdd9244d8d
Ryan Hollinger <dispose99-ryan@yahoo.com>
2017-12-15 22:23:00 -0500

    Updates database connection script to use const instead of define and mysqli_* functions instead of mysql_*

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,4 +1,5 @@
 <?php
+/*
 define('SQL_HOST','localhost');//database host
 define('SQL_USER','root');//username
 define('SQL_PASS','usbw');//password
@@ -9,4 +10,15 @@ $conn = mysql_connect(SQL_HOST, SQL_USER, SQL_PASS)
 
 mysql_select_db(SQL_DB, $conn)
   or die('Could not select database; ' . mysql_error());
+*/
+const SQL_HOST = "localhost";
+const SQL_USER = "root";
+const SQL_PASS = "usbw";
+const SQL_DB = "csc154_db";
+
+$conn = mysqli_connect(SQL_HOST, SQL_USER, SQL_PASS)
+  or die('Could not connect to the database; ' . mysqli_error());
+
+mysqli_select_db($conn, SQL_DB)
+  or die('Could not select database; ' . mysqli_error());
 ?>
